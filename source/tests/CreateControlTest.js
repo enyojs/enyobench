2b767cf2ab90b8e205592e7295c5fd6e361cfbc0
Ben Combee <ben.combee@lge.com>
2014-01-16 12:20:19 -0800

    Add Nested Control creation test
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@lge.com)

--- source/tests/CreateControlTest.js
+++ source/tests/CreateControlTest.js
@@ -14,3 +14,35 @@ enyoBench.speedKind({
 		this.testComplete();
 	}
 });
+
+enyoBench.speedKind({
+	name: "enyoBench.CreateNestedControlTest",
+	kind: "enyoBench.SpeedTest",
+	testName: "Nested enyo.Control Create & Destroy (x1000)",
+	view: enyo.kind({
+		kind: "enyo.Control"
+	}),
+	runTest: function() {
+		this.inherited(arguments);
+		for (var i = 0; i < 1000; ++i) {
+			var c = new enyo.Control({
+				components: [
+					{content: "I"},
+					{content: "II", components: [
+						{content: "a"},
+						{content: "b"},
+						{content: "c", components: [
+							{content: "1"},
+							{content: "2"}
+						]},
+						{content: "d"}
+					]},
+					{content: "III"},
+					{content: "IV"}
+				]
+			});
+			c.destroy();
+		}
+		this.testComplete();
+	}
+});
